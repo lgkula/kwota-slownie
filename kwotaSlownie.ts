892490f58e77fcbe3ca12bd52409f36aadae9a2b
Leslaw Kula <it@kula.wroclaw.pl>
2024-05-15 16:25:47 +0200

    first implementaion

--- kwotaSlownie.ts
+++ kwotaSlownie.ts
@@ -1,125 +1,148 @@
 const convertCurrencyAmountToNumber = (plnAmountText: string): number => {
-// const convertCurrencyAmountToNumber = (plnAmountText) => {
-  return parseFloat(plnAmountText.replace(",", ".").replace(/[^\d.-]/g, ""));
+  return parseFloat(plnAmountText.replace(',', '.').replace(/[^\d.-]/g, ''));
 };
 
-const splitFloatNumber = (decimalNumber: number) => {
-    let parts = decimalNumber.toString().split('.');
-    return [parseInt(parts[0]), parts[1] ? parseInt(parts[1]) : 0];
-}
+const splitFloatNumber = (decimalNumber: number): number[] => {
+  const parts = decimalNumber.toString().split('.');
+  return [parseInt(parts[0]), parts[1] ? parseInt(parts[1]) : 0];
+};
 
 const amountInWords = (amountAsString: string): string => {
-// const amountInWords = (amountAsString) => {
-  let unity = [
-    "",
-    " jeden",
-    " dwa",
-    " trzy",
-    " cztery",
-    " pięć",
-    " sześć",
-    " siedem",
-    " osiem",
-    " dziewięć",
+  const UNITY = [
+      '',
+      ' jeden',
+      ' dwa',
+      ' trzy',
+      ' cztery',
+      ' pięć',
+      ' sześć',
+      ' siedem',
+      ' osiem',
+      ' dziewięć',
   ];
-  let teen = [
-    "",
-    " jedenaście",
-    " dwanaście",
-    " trzynaście",
-    " czternaście",
-    " piętnaście",
-    " szesnaście",
-    " siedemnaście",
-    " osiemnaście",
-    " dziewiętnaście",
+  const TEEN = [
+      '',
+      ' jedenaście',
+      ' dwanaście',
+      ' trzynaście',
+      ' czternaście',
+      ' piętnaście',
+      ' szesnaście',
+      ' siedemnaście',
+      ' osiemnaście',
+      ' dziewiętnaście',
   ];
-  let ty = [
-    "",
-    " dziesięć",
-    " dwadzieścia",
-    " trzydzieści",
-    " czterdzieści",
-    " pięćdziesiąt",
-    " sześćdziesiąt",
-    " siedemdziesiąt",
-    " osiemdziesiąt",
-    " dziewięćdziesiąt",
+  const DOZENS = [
+      '',
+      ' dziesięć',
+      ' dwadzieścia',
+      ' trzydzieści',
+      ' czterdzieści',
+      ' pięćdziesiąt',
+      ' sześćdziesiąt',
+      ' siedemdziesiąt',
+      ' osiemdziesiąt',
+      ' dziewięćdziesiąt',
   ];
-  let hundreds = [
-    "",
-    " sto",
-    " dwieście",
-    " trzysta",
-    " czterysta",
-    " pięćset",
-    " sześćset",
-    " siedemset",
-    " osiemset",
-    " dziewięćset",
+  const HUNDREDS = [
+      '',
+      ' sto',
+      ' dwieście',
+      ' trzysta',
+      ' czterysta',
+      ' pięćset',
+      ' sześćset',
+      ' siedemset',
+      ' osiemset',
+      ' dziewięćset',
   ];
-  let groups = [
-    ["", "", ""],
-    [" tysiąc", " tysiące", " tysięcy"],
-    [" milion", " miliony", " milionów"],
-    [" miliard", " miliardy", " miliardów"],
-    [" bilion", " biliony", " bilionów"],
-    [" biliard", " biliardy", " biliardów"],
-    [" trylion", " tryliony", " trylionów"],
+  const GROUPS = [
+      ['', '', ''],
+      [' tysiąc', ' tysiące', ' tysięcy'],
+      [' milion', ' miliony', ' milionów'],
+      [' miliard', ' miliardy', ' miliardów'],
+      [' bilion', ' biliony', ' bilionów'],
+      [' biliard', ' biliardy', ' biliardów'],
+      [' trylion', ' tryliony', ' trylionów'],
   ];
 
-  let amountAsNumber: number = convertCurrencyAmountToNumber(amountAsString);
-  // let amountAsNumber = convertCurrencyAmountToNumber(amountAsString);
+  const amountAsNumber: number =
+      convertCurrencyAmountToNumber(amountAsString);
   if (amountAsNumber < 0) {
-    throw new Error("The amount must not be a negative value");
+      throw new Error('The amount must not be a negative value');
   }
-  if (amountAsNumber === 0) return "zero złotych";
 
-  const amountParts = splitFloatNumber(amountAsNumber);
-  console.log('amountParts: ', amountParts);
+  if (amountAsNumber === 0) return 'zero złotych';
 
-  let amountInWords = "";
-  let amountPartsInWords = [];
-  
+  const amountPartsNumber: number[] = splitFloatNumber(amountAsNumber);
+  if (amountPartsNumber[1] > 99) {
+      throw new Error(
+          'The amount given must not have more than two decimal digits',
+      );
+  }
+  if (amountPartsNumber[1] < 10) {
+      amountPartsNumber[1] = amountPartsNumber[1] * 10;
+  }
 
-  // if (amountParts[1] === 0) {
-  //   amountInWords = "złotych";
-  // } else {
-  //   amountInWords = "złotych " + amountParts[1] + "/100";
-  // }
-  amountParts.forEach((amountPart) => {
-    while (amountPart > 0) {
-      let g = 0;
-      let s = Math.floor((amountPart % 1000) / 100);
-      let n = 0;
-      let d = Math.floor((amountPart % 100) / 10);
-      let j = Math.floor(amountPart % 10);
+  const amountPartsInWords: string[] = [];
 
-      if (d == 1 && j > 0) {
-        n = j;
-        d = 0;
-        j = 0;
+  amountPartsNumber.forEach(amountPartNumber => {
+      if (amountPartNumber === 0) {
+          amountPartsInWords.push('zero');
+      } else {
+          let amountInWords = '';
+          let g = 0;
+          while (amountPartNumber > 0) {
+              const h = Math.floor((amountPartNumber % 1000) / 100);
+              let t = 0;
+              let d = Math.floor((amountPartNumber % 100) / 10);
+              let u = Math.floor(amountPartNumber % 10);
+
+              if (d == 1 && u > 0) {
+                  t = u;
+                  d = 0;
+                  u = 0;
+              }
+
+              let p = 2;
+              if (u == 1 && h + d + t == 0) p = 0;
+              if (u == 2 || u == 3 || u == 4) p = 1;
+              if (h + d + t + u > 0)
+                  amountInWords =
+                      HUNDREDS[h] +
+                      DOZENS[d] +
+                      TEEN[t] +
+                      UNITY[u] +
+                      GROUPS[g][p] +
+                      amountInWords;
+
+              g++;
+              amountPartNumber = Math.floor(amountPartNumber / 1000);
+          }
+          amountPartsInWords.push(amountInWords);
       }
-
-      let k = 2;
-      if (j == 1 && s + d + n == 0) k = 0;
-      if (j == 2 || j == 3 || j == 4) k = 1;
-      if (s + d + n + j > 0)
-        amountInWords =
-          amountInWords +
-          hundreds[s] +
-          ty[d] +
-          teen[n] +
-          unity[j] +
-          groups[g][k] 
-
-      g++;
-      amountPart = Math.floor(amountPart / 1000);
-    }
   });
 
-  console.log(amountInWords);
-  return amountInWords;
+  const addZlotyToAmount = (): string => {
+      if (amountPartsNumber[0] < 5) {
+          return `${amountPartsInWords[0]} złote`;
+      } else {
+          return `${amountPartsInWords[0]} złotych`;
+      }
+  };
+
+  const addGroszyToAmount = (): string => {
+      if (amountPartsNumber[1] === 0) {
+          return 'zero groszy';
+      } else if (amountPartsNumber[1] === 1) {
+          return `${amountPartsInWords[1]} grosz`;
+      } else if (amountPartsNumber[1] < 5) {
+          return `${amountPartsInWords[1]} grosze`;
+      } else {
+          return `${amountPartsInWords[1]} groszy`;
+      }
+  };
+  return `${addZlotyToAmount()}${addGroszyToAmount()}`;
 };
 
-amountInWords("1020340,45");
+console.log(amountInWords('1320320,20'));
